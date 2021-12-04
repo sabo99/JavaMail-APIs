457027dbb4c88a9201d0abfe442f97c7a61f9470
sabo99 <alvin.set99@gmail.com>
2021-12-04 12:29:57 +0700

    Java Mail APIs

--- app/src/main/java/com/sabo/javamailapis/Helpers/Credentials.kt
+++ app/src/main/java/com/sabo/javamailapis/Helpers/Credentials.kt
@@ -1,6 +1,6 @@
 package com.sabo.javamailapis.Helpers
 
 object Credentials {
-    const val EMAIL_SENDER = "email@gmail.com"
+    const val EMAIL_SENDER = "email"
     const val PASSWORD_SENDER = "password"
 }
